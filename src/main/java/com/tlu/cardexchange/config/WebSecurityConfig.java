3b534099ad33addd332d07653f4afdcad75c2dab
savanrof <64892863+savanrof@users.noreply.github.com>
2021-12-06 11:41:41 +0700

    change something

--- src/main/java/com/tlu/cardexchange/config/WebSecurityConfig.java
+++ src/main/java/com/tlu/cardexchange/config/WebSecurityConfig.java
@@ -1,63 +1,63 @@
-package com.tlu.cardexchange.config;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-@Configuration
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-  @Autowired
-  private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-
-  @Autowired
-  private UserDetailsService jwtUserDetailsService;
-
-  @Autowired
-  private JwtRequestFilter jwtRequestFilter;
-
-  @Autowired
-  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-    // Configure AuthenticationManager so that it knows from where to load user for
-    // matching credentials
-    // Use BCryptPasswordEncoder
-    // auth.userDetailsService(jwtUserDetailsService).passwordEncoder(passwordEncoder());
-  }
-
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
-
-  @Bean
-  @Override
-  public AuthenticationManager authenticationManagerBean() throws Exception {
-    return super.authenticationManagerBean();
-  }
-
-  /**
-   * add path
-   * 
-   * 
-   */
-  @Override
-  protected void configure(HttpSecurity httpSecurity) throws Exception {
-
-    httpSecurity.csrf().disable().authorizeRequests().antMatchers(HttpMethod.POST, "**/authenticate/**").permitAll().antMatchers("**/authenticate/**").permitAll().and().exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-    httpSecurity.cors();
-    // Add a filter to validate the tokens with every request
-    httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-  }
-
-}
+// package com.tlu.cardexchange.config;
+//
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.http.HttpMethod;
+// import org.springframework.security.authentication.AuthenticationManager;
+// import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+// import org.springframework.security.config.http.SessionCreationPolicy;
+// import org.springframework.security.core.userdetails.UserDetailsService;
+// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+// import org.springframework.security.crypto.password.PasswordEncoder;
+// import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//
+// @Configuration
+// @EnableWebSecurity
+// public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+// @Autowired
+// private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+//
+// @Autowired
+// private UserDetailsService jwtUserDetailsService;
+//
+// @Autowired
+// private JwtRequestFilter jwtRequestFilter;
+//
+// @Autowired
+// public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+// // Configure AuthenticationManager so that it knows from where to load user for
+// // matching credentials
+// // Use BCryptPasswordEncoder
+// // auth.userDetailsService(jwtUserDetailsService).passwordEncoder(passwordEncoder());
+// }
+//
+// @Bean
+// public PasswordEncoder passwordEncoder() {
+// return new BCryptPasswordEncoder();
+// }
+//
+// @Bean
+// @Override
+// public AuthenticationManager authenticationManagerBean() throws Exception {
+// return super.authenticationManagerBean();
+// }
+//
+// /**
+// * add path
+// *
+// *
+// */
+// @Override
+// protected void configure(HttpSecurity httpSecurity) throws Exception {
+//
+// httpSecurity.csrf().disable().authorizeRequests().antMatchers(HttpMethod.POST, "**/authenticate/**").permitAll().antMatchers("**/authenticate/**").permitAll().and().exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+// httpSecurity.cors();
+// // Add a filter to validate the tokens with every request
+// httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+// }
+//
+// }
