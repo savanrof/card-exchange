2d68457902820616e9393b8b449ce1900196246e
savanrof <64892863+savanrof@users.noreply.github.com>
2022-01-21 21:31:08 +0700

    commit

--- src/main/java/com/tlu/cardexchange/controller/MoneyController.java
+++ src/main/java/com/tlu/cardexchange/controller/MoneyController.java
@@ -1,9 +1,7 @@
 package com.tlu.cardexchange.controller;
 
 import javax.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,17 +11,14 @@ import com.tlu.cardexchange.service.MoneyService;
 @RestController
 @RequestMapping("/money")
 public class MoneyController {
-
   private final MoneyService moneyService;
 
-  @Autowired
   public MoneyController(MoneyService moneyService) {
     this.moneyService = moneyService;
   }
 
   @GetMapping("/")
-  @CrossOrigin
-  public ResponseEntity<MoneyDTO> getMoney(HttpServletRequest request) {
+  public ResponseEntity<MoneyDTO> getMonney(HttpServletRequest request) {
     return ResponseEntity.ok(moneyService.getMoney(request));
   }
 }
